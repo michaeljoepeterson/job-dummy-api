225ee5c3cf62de15f0bd872ad3b6bd77e5b96409
Michael Peterson <michaeljoepeterson@gmail.com>
2020-05-24 13:27:06 -0600

    initial commit-

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const {PORT} = require('./config');
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const app = express();
+app.use(jsonParser);
+//app.set('trust proxy', true);
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+  }
+  next();
+});
+
+function runServer(port = PORT) {
+    return new Promise((resolve, reject) => {
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  }
+  
+  function closeServer() {
+    return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    });
+  }
+  
+  if (require.main === module) {
+    runServer().catch(err => console.error(err));
+  }
+  
+  module.exports = { app, runServer, closeServer };

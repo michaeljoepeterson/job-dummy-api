86df5bf686abf55331ff944cb2a078553b4b96be
Michael Peterson <michaeljoepeterson@gmail.com>
2020-05-31 15:42:54 -0600

    added page 2 test data

--- routers/indeedRouter.js
+++ routers/indeedRouter.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const {indeedData} = require('./data');
+const {indeedData,indeedData2} = require('./data');
 
 router.get('/',(req,res) => {
+		let page = req.query.page;
+		let data = indeedData;
+		if(page == 2){
+			data = indeedData2
+		}
+
     return res.json({
         code:200,
-        data:indeedData
+        data
     }); 
 });
 
